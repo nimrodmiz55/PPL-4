9545705e7c6c1e9925caf5f9b768ef23edb8768c
Nimrod <nimi98791@gmail.com>
2026-06-21 16:18:04 +0300

    Q3.2.a

--- ex4.pl
+++ ex4.pl
@@ -15,8 +15,14 @@ maximum_printing_depth(100).
 % Signature: sub_list(Sublist, List)/2
 % Purpose: All elements in Sublist appear in List in the same order.
 % Precondition: List is fully instantiated (queries do not include variables in their second argument).
+% Example:
+% ?- sub_list(X, [1, 2, 3]).
+% X = [1, 2, 3] ; X = [1, 2] ; X = [1, 3] ; X = [2, 3] ;
+% X = [1] ; X = [2] ; X = [3] ; X = [] ; false.
 
-
+sub_list([], _).
+sub_list([H|SubTail], [H|ListTail]) :- sub_list(SubTail, ListTail).
+sub_list([SubHead|SubTail], [_|ListTail]) :- sub_list([SubHead|SubTail], ListTail).
 
 
 
